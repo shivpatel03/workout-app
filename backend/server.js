d9887f1e62d1947f3c066df262e41e4106639653
Shiv <da.shiv.patel@gmail.com>
2023-05-30 13:58:17 -0400

    Connected server to port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+// requirements
+require('dotenv').config()
+
+// imports
 const express = require('express')
+
+// creating express app
+const app = express()
+
+// route for main page
+app.get('/', (req,res) => {
+    res.json({msg: "Welcome to main page"})
+})
+
+// listen to a certain port number for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ' + process.env.PORT)
+})
