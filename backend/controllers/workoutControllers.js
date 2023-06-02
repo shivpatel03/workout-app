671c281749aeec7e9a38a9c1ee5e7d4188d2150b
Shiv <da.shiv.patel@gmail.com>
2023-06-02 16:22:35 -0400

    Created controllers for all requests

---
+++ backend/controllers/workoutControllers.js
@@ -0,0 +1,81 @@
+const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
+// get all workouts
+const getAllWorkouts = async(req,res) => { 
+    const allWorkouts = await Workout.find({}).sort({createdAt: -1}) // saving all workout to 'allWorkouts'
+    res.status(200).json(allWorkouts)
+}
+
+
+// get single workout
+const getOneWorkout = async(req,res) => {
+    const { id } = req.params
+    // make sure the id is a valid id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'Workout not found'})
+    }
+    const singleWorkout = await Workout.findById(id)
+    if (!singleWorkout) {
+        return res.status(404).json({error: 'Workout not found'})
+    }
+    else {
+        res.status(200).json(singleWorkout)
+    }
+}
+
+
+// post workout
+const createWorkout = async(req,res) => {
+    const {name, reps, weight, muscleGroup, difficulty} = req.body
+    try {
+        const workout = await Workout.create({name, reps, weight, muscleGroup, difficulty}) // the workout model
+        res.status(200).json(workout) 
+    }
+    catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+// delete workout
+const deleteWorkout = async(req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "Workout not found"})
+    }
+
+    const workoutToDelete = await Workout.findOneAndDelete({_id:id})
+    if (!workoutToDelete) {
+        return res.status(404).json({error: "Workout not found"})
+    }
+    else {
+        res.status(200).json(workoutToDelete)
+    }
+} 
+
+// update workout
+const updateWorkout = async(req,res) => {
+    const {id} = req.params
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "Workout not found"})
+    }
+    const workoutToUpdate = await Workout.findOneAndUpdate({_id:id}, {...req.body})
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "Workout not found"})
+    }
+    else {
+        res.status(200).json(workoutToUpdate)
+    }
+}
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+}
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,41 +1,29 @@
 // this file will include all routes related to workouts
 // GET all, GET one, POST a new workout, UPDATE, and DELETE a workout
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    getAllWorkouts,
+    getOneWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutControllers')
 
 const router = express.Router()
 
 // get all workouts
-router.get('/', (req,res) => { 
-    res.json({msg: "Get all workouts..."}) // dummy request for postman
-})
+router.get('/', getAllWorkouts)
 
 // get single workout
-router.get('/:id', (req,res) => {
-    res.json({msg: "Get workout with specific id"}) // dummy request for postman
-})
+router.get('/:id', getOneWorkout)
 
 // post workout
-router.post('/', async(req,res) => {
-    const {name, reps, weight, muscleGroup, difficulty} = req.body
-    try {
-        const workout = await Workout.create({name, reps, weight, muscleGroup, difficulty}) // the workout model
-        res.status(200).json(workout) 
-    }
-    catch (error) {
-        res.status(400).json({error: error.message})
-    }
-    res.json({msg: "Post a new workout to database"}) // dummy request for postman
-})
+router.post('/', createWorkout)
 
 // delete a workout
-router.delete('/:id', (req,res) => {
-    res.json({msg: "Delete a workout with given id"}) // dummy request for postman
-})
+router.delete('/:id', deleteWorkout)
 
 // delete a workout
-router.patch('/:id', (req,res) => {
-    res.json({msg: "Update a workout"}) // dummy request for postman
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
